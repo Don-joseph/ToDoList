15d628c88ec5a420ca03ddda5606e276323684b4
Don-joseph <107736392+Don-joseph@users.noreply.github.com>
2024-02-14 14:52:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://todolist-5345c-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+    
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    // push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+})
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
